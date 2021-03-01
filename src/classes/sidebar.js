886ebe01b700ec746d72e084fc76c14f6d9f0d35
okuznietsov <okuznitsov@teladoc.com>
2021-03-01 14:35:45 +0200

    Add project

---
+++ src/classes/sidebar.js
@@ -0,0 +1,51 @@
+import { block } from '../utils';
+import { TextBlock, TitleBlock } from './blocks';
+
+export class Sidebar {
+    constructor(selector, updateCallback) {
+        this.$el = document.querySelector(selector);
+
+        this.update = updateCallback;
+
+        this.init();
+    }
+
+    init() {
+        this.$el.insertAdjacentHTML('afterbegin', this.template);
+        this.$el.addEventListener('submit', this.add.bind(this))
+
+    }
+
+    get template() {
+        return [
+            block('text'),
+            block('title')
+        ].join('')
+    }
+
+    add (event) {
+        event.preventDefault();
+
+        const type = event.target.name;
+        const value = event.target.value.value;
+        const styles = event.target.styles.value;
+
+        let newBlock;
+
+        switch (type) {
+            case 'text':
+                newBlock = new TextBlock(value, {styles});
+                break;
+            case 'title':
+                newBlock = new TitleBlock(value, {styles});
+                break;
+            default:
+                return '';
+        }
+
+        this.update(newBlock);
+
+        event.target.value.value = '';
+        event.target.styles.value = '';
+    }
+}
